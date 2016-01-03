abc5f2b48e20f8cc651e9fe70854b4762fe0b36d
kiranvsk1 <kiran.vsk1@gmail.com>
2016-01-03 13:07:36 +0530

    add todos n TodoID

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todo =[{
+  id: 1,
+  description:"Nothing",
+  completed : false
+},
+{
+  id: 2,
+  description:"Nothing To Do",
+  completed : false
+}];
+
 
 app.get('/',function (req,res) {
   res.send('This is Todo Api Working');
+})
+
+app.get('/todos',function (req,res) {
+  res.json(todo);
+})
+app.get('/todo/:id',function (req,res) {
+  var matchedTodo;
+  todo.forEach(function (obj) {
+    if (obj.id === parseInt(req.params.id,10)) {
+      matchedTodo = obj;
+    }
+  });
+  if (matchedTodo) {
+    res.json(matchedTodo);
+  } else {
+    res.status(404).send();
+  }
 })
 app.listen(PORT,function () {
   console.log("express working on "+ PORT + "!");
